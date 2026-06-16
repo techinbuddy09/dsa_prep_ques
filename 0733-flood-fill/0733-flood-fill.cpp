d079ba9eab2cb8002e14ab9ad4f9a7f4ac0aac56
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-06-16 17:34:28 +0530

    Time: 0 ms (100%), Space: 17.9 MB (90.58%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,35 +1,31 @@
 class Solution {
-    private:
-    //always pass by refernece else will create copy
-        void dfs(int r,int c,vector<vector<int>>&image,vector<vector<int>>&vis,int dr[],int dc[],int new_col , int i_c)
+    void dfs(int r,int c,vector<vector<int>>&image,vector<vector<int>>&ans,int dr[],int dc[],int new_color,int i_c)
+    {
+        int n = image.size();
+        int m = image[0].size();
+        //initial configuration
+        ans[r][c] = new_color;
+        //next traverse on the adjacnet and if they are not yet visited do the color change on them to recursilvey and also the adjacent should be e to the color of initial color
+        for(int i = 0;i<4;i++)
         {
-            vis[r][c]=new_col;//filling with the new color
-           int n=image.size();
-           int m=image[0].size();
-           //now iterating on the adjacent ones
-           //iterating in the  four directions
-           for(int i=0;i<4;i++)
-           {
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-            //now condition check+rest other if adjacent equals to i_c and not visited too
-            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc] == i_c && vis[nr][nc]!=new_col)
+            int nr = r+dr[i];
+            int nc = c+dc[i];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && ans[nr][nc] != new_color && image[nr][nc] == i_c)
             {
-                dfs(nr,nc,image,vis,dr,dc,new_col,i_c);
+                // do the further dfs call
+                dfs(nr,nc,image,ans,dr,dc,new_color,i_c);
             }
-           }
-
         }
+    }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-
-        int i_c=image[sr][sc];// the initial color
-        //vis array for the replica array
-        vector<vector<int>>ans=image;//creation of the replica array
-        int dr[]={-1,0,1,0};
-        int dc[]={0,-1,0,1};
-        //call the dfs 
-        dfs(sr,sc,image,ans,dr,dc,color,i_c);
-        return ans;
-      }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+        int initial_color = image[sr][sc];
+        vector<vector<int>>ans = image ;// copy of the image array so that we can do the modifications here of the color
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        //now here lets do the dfs call  here
+        dfs(sr,sc,image,ans,dr,dc,color,initial_color);
+        return ans;//return the modified ans vector
+                         }
 };
