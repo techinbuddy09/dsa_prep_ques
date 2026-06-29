f54d7fe93ee6b43e93d4304e0019dd470cd97712
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-06-29 22:00:43 +0530

    Time: 12 ms (71.87%), Space: 17 MB (58.97%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -2,58 +2,55 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<int>>dis(n,vector<int>(n,1e9));
-        //creation of the graph
+        // graph creation of assigning the edge weights
         for(auto it:edges)
         {
-            dis[it[0]][it[1]]=it[2];
-            dis[it[1]][it[0]]=it[2];
+            dis[it[0]][it[1]] = it[2];
+            dis[it[1]][it[0]] = it[2]; // here in accountance we are taking the undorected graph here
         }
-        //now same node to node
+        // next we will assign the digonal
         for(int i=0;i<n;i++)
         {
-            dis[i][i]=0;
+            dis[i][i]=0;// we mark it 0
         }
-        //now applying the floydd warshalls algo
-        for(int via=0;via<n;via++)
+
+        // now perform the floyd warshall
+        for(int via =0 ;via<n;via++)
         {
-            for(int i=0;i<n;i++)
+            for(int i =0;i<n;i++)
             {
-                for(int j=0;j<n;j++)
+                for(int j =0;j<n;j++)
                 {
                     if(dis[i][via] == 1e9 || dis[via][j] == 1e9)
                     {
-                        continue;
+                        continue;// here u just simply count it
                     }
-                    dis[i][j]=min(dis[i][j],dis[i][via]+dis[via][j]);
+                    dis[i][j] = min(dis[i][j],dis[i][via]+dis[via][j]);
                 }
             }
         }
 
-        //now  final checking
-        int city_count=n;
-        int city_no=-1;
-        //basically city to adjacent city
-        for(int city=0;city<n;city++)
+        // next main logic
+        int maxi_count = n;
+        int city_no = -1;
+        //lets iterate in the city and then next to its adjacent cities and for each city count the number of neighbouring cities whose threshold within disthreshold
+        for(int city = 0;city<n;city++)
         {
-            int c=0;
-            for(int adjcity=0;adjcity<n;adjcity++)
+            int c = 0;
+            for(int adjcity = 0;adjcity<n;adjcity++)
             {
-                //the check of condition part  if the  curr city has its adjacent cities within the threshold_distance  increase count
-                if(dis[city][adjcity] <= distanceThreshold)
-                 {
+                if(dis[city][adjcity] <=  distanceThreshold) // we are taking both the cases <=
+                {
                     c++;
-                 }  
-               // c++;
+                }
             }
-
-            //now updation part
-            if(c<=city_count)
+            // now here do check and updates
+            if(c<= maxi_count)
             {
-                city_count=c;
-                city_no=city;//the the city with smaller number of adjacents under threshold 
+                maxi_count=c;
+                city_no = city;
             }
         }
-    return city_no;//return the final city
-
+        return city_no;
     }
 };
