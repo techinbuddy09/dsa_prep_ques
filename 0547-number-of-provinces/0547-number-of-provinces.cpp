00b2b794ea3ec88c7c8bce2bf2b880cf184ffd36
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-07-23 11:15:52 +0530

    Time: 0 ms (100%), Space: 19.6 MB (61.19%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,44 +1,80 @@
-class Solution {
-    void dfs(int node,vector<int>adj[],vector<int>&vis)
-    {
-        // first mark  the node visited
-        vis[node] = 1;
-        //next call dfs on its adjacent neightbours
-        for(auto it: adj[node])
-        {
-            if(!vis[it]) // if not visited then only
+class djs
+{
+    //firstly the parent and size array
+    vector<int>parent,size;
+    public:
+            djs(int n) //constructor
             {
-                dfs(it,adj,vis);
+                parent.resize(n+1);
+                size.resize(n+1);
+                //now lets iterate and initialize the values
+                for(int i = 0;i<=n;i++)
+                {
+                    parent[i] = i;
+                }
+
+                // size array
+                for(int i =0;i<=n;i++)
+                {
+                    size[i] = 1;
+                }
             }
-        }
-    }
+
+            int findupar(int node) //
+            {
+                if(node == parent[node])
+                        return node;
+                return parent[node] = findupar(parent[node]);        
+            }
+
+            void union_bysize(int u , int v)
+            {
+                // firstly finding the ultimate parents
+                int ulp_u = findupar(u);
+                int ulp_v = findupar(v);
+                
+                if(ulp_u == ulp_v) return ;// since same component
+                else if(size[ulp_u] < size[ulp_v])
+                {
+                    parent[ulp_u] = ulp_v;
+                    size[ulp_v]+=size[ulp_u];
+                }
+
+                else
+                {
+                    parent[ulp_v] = ulp_u;
+                    size[ulp_u]+=size[ulp_v];
+                }
+            }
+};
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        //adjacency matrix to adjacency list conversion
-       vector<int>adj[n+1];
-       for(int i = 0;i<n;i++)
-       {
+        djs ds(n);
+        //lets iterate in the matrix
+        for(int i = 0;i<n;i++)
+        {
             for(int j = 0;j<n;j++)
             {
-                if(isConnected[i][j] == 1 && i!=j)
+                if(isConnected[i][j] == 1)
                 {
-                    adj[i+1].push_back(j+1);
-                    adj[j+1].push_back(i+1);
+                      ds.union_bysize(i,j); // its like if there exists an edge then only we gonna merge
                 }
             }
-       }
+        }
         int count = 0;
-        vector<int>vis(n+1,0);
-        for(int i = 1;i<=n;i++)
+        // now lets iterate and find the count of the unique ultimate parents
+        //here we gonna iterate on all and check if ultimate boss then only increase count
+        for(int i = 0;i<n;i++)
         {
-            if(!vis[i])
+            if(ds.findupar(i) == i)
             {
-                //dfs(i,adj,vis);
                 count++;
-                dfs(i,adj,vis);
             }
         }
         return count;
+
+        
     }
 };
