7048808417595d68a07009b3105ce96c7c7b1497
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-07-26 23:31:55 +0530

    Time: 0 ms (100%), Space: 40.3 MB (61.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -26,3 +26,42 @@ public:
         return ans;
     }
 };
+
+// product array is like for each idx we multiply all elements except the current nums
+
+// here we can compute the left array and the right array
+// and then next for each index left array * right array
+
+
+/*vector<int> left(n);
+vector<int> right(n);
+vector<int> ans(n);
+
+left[0] = 1;
+for(int i = 1; i < n; i++)
+{
+    left[i] = left[i-1] * nums[i-1]; // not including number itself isliya i-1
+}
+
+[1,2,3,4]  =>[1,1,2,6]
+
+right[n-1] = 1;
+for(int i = n-2; i >= 0; i--)
+{
+    right[i] = right[i+1] * nums[i+1];
+}
+
+[1,2,3,4] =>[24,12,4,1]
+
+for(int i = 0; i < n; i++)
+{
+    ans[i] = left[i] * right[i];
+}
+
+[1,2,3,4]=>[24,12,8,6] done hehe
+
+// optimization like now not using separate left and right array
+means basically firstly we eliminated extra left array creation by using our ans array as left array only next as we dont want the entire right array atb once so instead of creating the entire right array at once we maintained a variable right initially with 1 next we start iterating in nums theek and firsly in the value at the right array index we multiply it by right variable value next update the right variable index value with value in nums[i] and here we are iterating nums frommbackward so in this way we have reduced our extra space complexity of creating two separate left and right arrays hehe
+
+*/
+
