39eb3561fd58cda5fe26b75c67c5c0b0ad847bad
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-07-27 18:46:40 +0530

    Time: 3 ms (68.19%), Space: 65.1 MB (69.97%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>mp;
-        int ans = -1;
-        //lefts insert everything in map
-        for(int& no:nums)
-        {
-            mp[no]++;
+        
+        //here in this question cycle toh we will be having yr 
+        //toh phase 1 is to find any meeting point in the cycle
 
-            if(mp[no]>1)
-            {
-                return no;
-                break;
-            }
-        }
-        return -1;
+        //we point to the next index value
+         int slow = nums[0];
+         int fast = nums[0];
+
+         do
+         {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+         }while(slow!=fast);// here we are using do-while loop
+
+         //next phase 2 wherein we gonna try to find the entry point of the cycle
+         slow = nums[0];
+         while(slow!=fast)
+         {
+            slow = nums[slow];
+            fast= nums[fast];
+         }
+         return slow;
+       
     }
 };
-// here we gonna learn the algorithm Floyd's Cycle Detection
