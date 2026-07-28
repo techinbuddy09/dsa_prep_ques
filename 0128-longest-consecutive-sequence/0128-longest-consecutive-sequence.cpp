63e1e12c1cbe7ba34defa34aed470500dd31207a
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-07-28 20:00:59 +0530

    Time: 86 ms (34.4%), Space: 88.8 MB (76.27%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n=nums.size();
-        if(n == 0)
-            return 0;//base case return 0 if no elements 
-        unordered_set<int>st;
-        int longest=1;
-        for(int i=0;i<n;i++)
+
+        if(nums.empty())
+            return 0;
+
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int longest = 0;
+
+        for(int x : st)
         {
-            st.insert(nums[i]); //insert elemnts from nums array to the set
-        }    
-        //reiterate in set
-        for(auto it:st)
-        {
-            //firstly see if ele not in set
-            if(st.find(it-1) == st.end())
+            if(x == INT_MIN || st.find(x - 1) == st.end())
             {
-                int count=1;
-                int x=it;//store the current element
-                // now next agin loop till the cinsecutive of that element
-                while(st.find(x+1)!= st.end())
+                int curr = x;
+                int len = 1;
+
+                while(curr < INT_MAX && st.find(curr + 1) != st.end())
                 {
-                    x=x+1;
-                    count=count+1;
-                }    
-                //here only calculate the longes consecutive sequence
-                longest=max(longest,count);                         
+                    curr++;
+                    len++;
+                }
+
+                longest = max(longest, len);
             }
         }
-        return longest;//return the longest consecutive sequence 
+
+        return longest;
     }
 };
-//basically in this using  the set data structured the unordered one
