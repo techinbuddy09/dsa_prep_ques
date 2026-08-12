c90eabb6803da437f9026349bea1aeacfb2ef41e
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-08-13 02:02:43 +0530

    Time: 0 ms (100%), Space: 18 MB (68.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+
         int n = nums.size();
-        // sabse phele we will make the frequency map
+        // firstly the frquency map
         unordered_map<int,int>mp;
         for(int no:nums)
         {
-            mp[no]++;//done with the frequency map
+            mp[no]++;
         }
 
-        // next lets make the min heap of pair type
+        //next push in min heap and pop the small omes
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        //lets iterate in the frequency map and push in pq
+        //lets iterate in the map and push in pq
         for(auto it:mp)
         {
-            pq.push({it.second,it.first});
-            if(pq.size() > k)
-            {
-                pq.pop();//if the size it exceeeds the the k size we need to pop
-            }
+            pq.push({it.second,it.first});//we gonna push {freq,element}
+
+            // next also we have to maintain the size of the min heap that it should be of size k na
+             if(pq.size() > k)
+             {
+                pq.pop();
+             }
         }
 
-        // next we gonna reiterate and push the elements in the vector till the pq its not empty
-        vector<int>ans;
+        // next now by this time our priority queue holding the top k most frequent elements here
 
+        // lets push them in our vector and return it
+        vector<int>res;
+
+        //push elements in the vector till the pq not empty
         while(!pq.empty())
         {
-            ans.push_back(pq.top().second);
+            res.push_back(pq.top().second);
             pq.pop();
         }
 
-        return ans;
+        return res;
 
-        //t.c 
-        //traversing in the hashmap => O(n)
-        // for the heap operations => O(mlogk)
-        
     }
 };
+// we have to basically return the k most frequent elements over here
+//means 2 most frequent elements
+// as the question states here na top k frequent elements na means top 2 most frequent elements na
+// so basically here jin doh elements ki frequency top 2 frequents mein aye
