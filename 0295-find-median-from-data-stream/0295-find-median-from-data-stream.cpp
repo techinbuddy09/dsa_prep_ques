8a11cde3af599cf9b42cd1d450f1863ae835b998
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-08-14 15:46:37 +0530

    Time: 42 ms (98.88%), Space: 148.7 MB (30.28%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,50 @@
+class MedianFinder {
+    // here in this ques we are going to have basically two heaps 
+    //one will be our  maxheap and other will be minheap
+    priority_queue<int>left_max;
+    priority_queue<int,vector<int>,greater<int>>right_min;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        // lets firstly add the number in the heap
+        if(left_max.empty()||num<=left_max.top())
+        {
+            left_max.push(num);
+        }
+        else
+        {
+            right_min.push(num);
+        }
+
+        // next after inserting we also need to maintain balance
+        if(left_max.size() > right_min.size()+1)
+        {
+            //means left has two extra elements
+            right_min.push(left_max.top());
+            left_max.pop();
+        }
+
+        if(right_min.size() > left_max.size())
+        {
+            left_max.push(right_min.top());
+            right_min.pop();
+        }
+        // alright so done with balancing
+        
+    }
+    
+    double findMedian() {
+
+        if(left_max.size() == right_min.size())
+        {
+            return (double)((left_max.top()+right_min.top())/2.0);
+        }
+        else
+            return left_max.top();
+        
+    }
+};
+
