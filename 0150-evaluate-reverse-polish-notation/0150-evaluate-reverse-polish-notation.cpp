00df58886b3a02d5baf391d3b28db9e910e192f2
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-08-19 14:40:28 +0530

    Time: 0 ms (100%), Space: 17 MB (66.8%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,41 +1,59 @@
 class Solution {
-    int operate(int a,int b,string tok)
-    {
-        if(tok == "+")
-            return a+b;
-        if(tok == "-")
-            return a-b;
-        if(tok == "*")
-            return (long)a*(long)b;
-        if(tok == "/")
-            return a/b;
-        else
-            return -1;//like if nothing            
-    }
 public:
     int evalRPN(vector<string>& tokens) {
-        //performing postfix calculations
-        stack<int>st;
-        for(auto it:tokens)
-        {
-            if(it == "+" || it == "-" || it == "*" || it == "/")
+        
+          // firstly we gonna iterate and thta too on string like andar sare strings ha charcters nahi ha
+          stack<int>st;
+          for(string  ch:tokens)
+          {
+            //case-1 check for operators
+            if(ch == "+" || ch == "-" || ch == "/" || ch == "*")
             {
-                // jaise hee we hit an operator we pop the two operands and perform operation
-                int b = st.top();//right
+                int b = st.top();
                 st.pop();
+
                 int a = st.top();
-                st.pop();//left;
-                int res = operate(a,b,it);
-                // after performimg operation we need to push th result back in the stack
-                st.push(res);
+                st.pop();
+
+                if(ch == "+")
+                    st.push(a+b);
+                else if(ch == "-")
+                    st.push(a-b);
+                else if(ch == "*")
+                    st.push(a*b);
+                else 
+                    st.push(a/b); 
             }
             else
             {
-                // push the elements in stack
-                st.push(stoi(it));// convert the string to the integer over here
+                //case 2 encountered that its number so firstly convert the string into number and then directly push it in stack
+                st.push(stoi(ch));
             }
-        }
-
-        return st.top();// the stack top will be holding the answer
+          }
+          return st.top();// it will holding our ans
     }
 };
+/* here  in this ques  we gonna use stack theek
+
+number → push
+operator → pop two values → calculate → push result
+
+4  13  5  /  +
+↓  ↓   ↓   ↓
+push push push
+          ↓
+       13 / 5 = 2
+          ↓
+        push 2
+          ↓
+        4 + 2
+          ↓
+          6
+alright so the basic intution of the ques it goes like whenever a number is seen we simply push it the stack
+=>next whenever a operator is seen we pop the two last push  numbers in stack
+=> a = first popped
+=> b  = second popped
+=>for handling the order we have b(op)a 
+=>alright after performing the operation we push the result back in the stack
+=> and then repeat the entire process
+*/
