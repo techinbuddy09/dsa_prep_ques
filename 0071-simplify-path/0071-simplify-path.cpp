7e5ec1f701872e1920cb0312e2414d6a81ddfb51
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-08-27 19:59:59 +0530

    Time: 3 ms (63.73%), Space: 15.1 MB (19.91%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,35 +1,69 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        string token = "";
-        stringstream ss (path);
+        
+        // firstly creating a stack of strings over here
         stack<string>st;
-        while(getline(ss,token,'/'))
+        stringstream ss(path);
+        string copy;//to store the components or peices of strings separated by delimeter
+        while(getline(ss,copy,'/'))
         {
-            if(token == "." || token == "")
+            if(copy == "" || copy == ".")
             {
-                continue;// dont do anything
+                continue;
             }
-            if(token != "..")
+            else if(copy == "..")
             {
-                st.push(token);
+                //here if the stack is not empty then only pop
+                if(!st.empty())
+                {
+                    st.pop();
+                }
             }
-            else if(!st.empty())
+
+            else
             {
-                st.pop();// if not empty then pop
+                st.push(copy);
             }
         }
-        //edge case if stack empty return root directory
-        if(st.empty())
+
+        // now in stack we have our components now we need to do the formation 
+        string ans ="";
+        while(!st.empty())
+        {
+           ans = "/"+st.top()+ans;
+           st.pop();
+        }
+
+        // edge case if the string is empty then return the root 
+
+        if(ans  == "")
         {
             return "/";
         }
-        string rev ="";
-        while(!st.empty())
-        {
-            rev="/"+st.top()+rev;
-            st.pop();
-        }
-        return rev;
+        return ans;
     }
 };
+/* okay so our task is to simplify the unix style path into simplified canonical path
+
+=>
+normal name → PUSH {simple directory}
+"."         → IGNORE {like same directory na}
+".."        → POP (like go back to the previous )
+""          → IGNORE (agar empty ho toh ignore it)..something like {///}
+
+path
+ ↓
+split into components
+ ↓
+normal folder → push
+"." → ignore
+".." → pop if possible
+empty → ignore
+ ↓
+stack = ["a", "c"]
+ ↓
+join using "/"
+ ↓
+"/a/c"
+*/
