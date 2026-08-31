a3e5603c4d6c3c2936b6a55b3cb82e727f8a4005
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-08-31 19:11:24 +0530

    Time: 0 ms (100%), Space: 15 MB (26.39%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,35 +11,56 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* slow = head;
-        ListNode* fast = head;
+        
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* slow  =  dummy;
+        ListNode* fast = dummy ;
 
-        // firstly we gonna move fast by the n steps
-        for(int i = 0 ;i<n;i++)
+        //lets move fast by steps to create n gaps between the two pointers 
+        for(int i = 0 ;i<n; i++)
         {
             fast = fast->next;
         }
 
-        //suppose if ourhead is the node we have to delete
-        if(fast == NULL)
-        {
-            ListNode* delnode = head;
-            head = head->next;
-            delete(delnode);
-            return head;
-        }
-
-        // if not we gonna traverse
+        //next lets move the pointers till one reach null
         while(fast->next!=NULL)
         {
-            slow = slow->next;
+            slow  = slow->next;
             fast = fast->next;
         }
 
-        ListNode* del = slow->next;
+        //next lets do chnages of the links over here theek
         slow->next = slow->next->next;
-        delete(del);
-        return head;
-        
+
+
+        // next return dummy->next ..because it points to head na
+        return dummy->next;
+
     }
 };
+/*we have to basically remember Keep slow and fast n positions apart. Move both until fast->next == NULL. Now slow is exactly one node before the target. Skip slow->next.
+
+1. Dummy banao
+   ↓
+   dummy->next = head
+
+2. slow = dummy
+   fast = dummy
+
+3. fast ko n steps aage move karo
+   → n ka gap create
+
+4. slow + fast dono ko move karo
+   → jab tak fast->next == NULL
+
+5. Ab slow target se ONE NODE pehle hai
+
+6. Target ko skip karo
+   → slow->next = slow->next->next
+
+7. dummy->next return karo
+
+
+the basic reason for creating a dummy node here is that if suppose the target which we have to delete is the head only toh ..toh agar usse hi delete kardiya how we will  return our list .. thats the reason we have created a dummy one here an artifically node which points head .. in this way we can remove our head nodes like other linked lists
+*/
