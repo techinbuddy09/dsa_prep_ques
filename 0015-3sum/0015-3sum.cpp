9b23e8c21e59529d36fde29210360e2168be2ddf
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-09-01 22:32:36 +0530

    Time: 49 ms (48.44%), Space: 29.1 MB (73.19%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>res;
         
-        //sort nums array
+        int n = nums.size();
+        //next we gonna sort the array
         sort(nums.begin(),nums.end());
-        // fix the first element
-        for(int i = 0;i<n;i++)
+
+        vector<vector<int>>ans;
+        //lets iterate 
+        for(int i =0 ;i<n;i++)
         {
-            //firstly we will skip the duplicate fix element
+            //firstly skip the duplicates for the fixed elements
             if(i>0 && nums[i] == nums[i-1])
-                    continue;
-            //next lets set two pointer
+            {
+                continue;
+            }
+
+            //next we gonna set the two pointers here theek
             int left = i+1;
             int right = n-1;
 
             while(left<right)
             {
-                int sum = nums[i]+nums[left]+nums[right];
+                int sum = nums[i]+nums[left]+nums[right];// waise our target is 0 theek and here nums[i] is the fix element which we keep one by one and calc
                 if(sum < 0)
                 {
+                    //need bigger move left
                     left++;
                 }
                 else if(sum > 0)
@@ -29,28 +34,25 @@ public:
                 }
                 else
                 {
-                    //means found a triplet where the sum is 0 with fixing that one element
-                    res.push_back({nums[i],nums[left],nums[right]});
-                    //next to avoid duplicates move the both pointers
+                    ans.push_back({nums[i],nums[left],nums[right]});
                     left++;
                     right--;
-                    // also case to avoid ineer loop duplicates
-                    while(left<right && nums[left] == nums[left-1]) // like same value on pointer to if we use again it will give same ans means duplicates so we need to avoid it
+
+                    //now before moving to other we need to skip the duplicates for left and right
+                    while(left<right && nums[left] == nums[left-1])
                     {
                         left++;
                     }
 
-                     while(left<right && nums[right] == nums[right+1]) // like same value on pointer to if we use again it will give same ans means duplicates so we need to avoid it
+                    while(left<right && nums[right] == nums[right+1])
                     {
                         right--;
-                    }
-
+                    } 
                 }
-            }        
+            }
+
         }
 
-        return res;// the final vector storing the unique triplets
-
-        
+        return ans;
     }
 };
