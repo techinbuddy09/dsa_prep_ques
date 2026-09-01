e84aeff5fdfab941d43ba049a55f90c1167229ed
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-09-01 21:12:31 +0530

    Time: 0 ms (100%), Space: 14.8 MB (43.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // alright so we gonna do this ques by the optimal one approach which is hashing
+
+        //here we calc the remaiing value of our target and try to locate in hashmap
+        //if found return indicies of tha {remaining ele indicies , current_ele indicies}
+
         int n = nums.size();
         unordered_map<int,int>mp;
-        // no :idx
-        for(int i=0;i<n;i++)
+        for(int i = 0 ;i<n;i++)
         {
-            int rem  =  target - nums[i];
+            int rem = target - nums[i];
+            // now locate in map if found its good  we return the indicies 
+            //if not update the value in map
             if(mp.find(rem)!= mp.end())
             {
-                // return idx of {prev ele,curr_ele}
                 return {mp[rem],i};
             }
-            //else update 
+            //if not we need to update it
             mp[nums[i]] = i;
         }
-        
-        // if nothing 
+        //and if nothing is found ..we  gonna return nothing 
         return {-1};
+        
     }
 };
-// solving two_sum by hashing
+
+// solved the ques using t.c = O(n) ..like the t.c to look for elements in the hash map O(1) best and worst is O(n)
