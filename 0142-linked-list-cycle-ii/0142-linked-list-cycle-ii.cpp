a6b64cea95dd8eeac45ff7ef20a3f4f56d040092
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-09-01 14:44:11 +0530

    Time: 7 ms (65.83%), Space: 11.4 MB (54.95%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,35 +9,51 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        //firstly the detection of cycle
         ListNode* slow = head;
         ListNode* fast = head;
+        // firstly cycle detection
         while(fast!=NULL && fast->next!=NULL)
         {
-            slow = slow->next;
+            slow = slow ->next;
             fast = fast->next->next;
-            // now if cycle detected then
-            if(slow == fast)
+
+            if(slow == fast) // if point of collsion found toh rest it
             {
-                // we gonna rest the pointer 
                 slow = head;
-                // and keep the fast pointer at collision point only
-                // next traverse till slow !=fast
+
+                // next move both by 1-1 step only
                 while(slow!=fast)
                 {
-                    // move both the pointer by one step
                     slow = slow->next;
                     fast = fast->next;
                 }
-
-                // now here if slow ==  fast yes its the starting point
-                if(slow == fast)
+                    if(slow == fast) //means entrance found here
+                    {
                         return slow;
-                
+                    }
             }
         }
-        // if nothing works 
+        // if nothing works in that case return null
         return NULL;
         
     }
 };
+/* 
+   alright so the mofification which we have in this ques is that in this ques we have to return the starting point of the cycle .. if cycle exists
+
+1️⃣ Slow + Fast → find collision
+
+2️⃣ Collision found
+
+3️⃣ Move slow back to head
+
+4️⃣ Move both slow and fast ONE step at a time
+
+5️⃣ Where they meet again = cycle starting node
+
+=> as a notes we can add it here
+
+imp intuition -: The reason is that after the first collision, the distance from head to the cycle start is equal to the distance from the collision point around the cycle to the cycle start
+
+That's why putting one pointer at head and moving both one step makes them meet exactly at the cycle entrance.
+*/
