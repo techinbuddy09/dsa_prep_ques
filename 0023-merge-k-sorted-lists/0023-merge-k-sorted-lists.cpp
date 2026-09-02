4be7e7d509ca1de2b0bfc52527b6dfc535f79728
techbuddy09 <147515340+techinbuddy09@users.noreply.github.com>
2026-09-02 17:59:54 +0530

    Time: 0 ms (100%), Space: 18.7 MB (33%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -12,41 +12,37 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        //here in this question we gonna maintain min heap as we want to mege the k sorted linked lists in the sorted format
         priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
-
-        //next lets iterate on each linked list push current node val of each list in the pq ...means the first value of each list...first value always sorted
-
-        for(auto list:lists)
+        ListNode*dummy = new ListNode(-1);
+        ListNode* tail = dummy;// for traversing 
+        //next lets firstly iterate in each list and push the current element and node 
+        for( auto list:lists)
         {
-            if(list!=NULL) // wahi basicaaly our condition states that if the list not empty toh push the list ki val aur list ...and similary traverson on the other k list over here theek
+            //if value there then only add
+            if(list!=NULL)
             {
                 pq.push({list->val,list});
-            }   
+            }
         }
-
-        ListNode* dummy = new ListNode(-1);
-        ListNode* tail = dummy;// basically using this list for traversing 
-
-        // now lets pop  from pq [node->val,node]
+        //lets pop and connect and update
         while(!pq.empty())
         {
             auto[val,node] = pq.top();
             pq.pop();
 
-            //lets add the ans first
+            //next do connection 
             tail->next = node;
-            // move the pointer 
-            tail = tail->next;
+            tail = tail->next;//move the pointer
 
-            //next if the node ka exists the push the next value of the linked list into the pq
+            // also need to update too
             if(node->next!=NULL)
             {
                 pq.push({node->next->val,node->next});
             }
         }
-   return dummy->next;
 
+        return dummy->next;
+
+        
     }
 };
-
